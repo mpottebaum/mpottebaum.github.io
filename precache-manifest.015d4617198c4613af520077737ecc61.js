c3f91c6669526ce4b4367b9e13372974091ccbb7
mpottebaum <mpottebaum@gmail.com>
2020-08-04 13:13:39 -0500

    Updates

---
+++ precache-manifest.015d4617198c4613af520077737ecc61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6dbfa258f685a1938c83587ef2f200e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5ec10049a46e25c3f63",
+    "url": "/static/css/main.cba07218.chunk.css"
+  },
+  {
+    "revision": "34615f723b416f00563a",
+    "url": "/static/js/2.f7b4cb40.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7b4cb40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ec10049a46e25c3f63",
+    "url": "/static/js/main.ac637ade.chunk.js"
+  },
+  {
+    "revision": "0410a8d8f44e89eab149",
+    "url": "/static/js/runtime-main.6aee4db5.js"
+  }
+]);
